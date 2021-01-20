297208c198c6933475129733c119bb24b21455ef
Mayu Miyoshi <mayu.miyoshi1313@gmail.com>
2021-01-20 23:43:00 +0900

    Dose created

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,20 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+require 'open-uri'
+puts 'Destroying data...'
+Ingredient.destroy_all
+puts 'Data destroyed!'
+puts 'Generating new data...'
+
+ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+serialized_ingredients = open(ingredients_url).read
+
+ingredients = JSON.parse(serialized_ingredients)['drinks']
+
+ingredients.each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
+puts 'All data generated!'
